3dbd1fca6b58fb9d6d7eb55cd4dc4519be79b729
Rakshith <werakshith@gmail.com>
2023-08-07 09:40:33 +0530

    Neuralnetworks.py
    Building a neural network for a XOR gate.(Get better understanding of vectorized implementation of operations, and generalizing).

--- Neuralnetworks.py
+++ Neuralnetworks.py
@@ -1,58 +1,98 @@
 import numpy as np
+from pprint import pprint
 
-class layer_data():  # This is a data class, I've stored data of each neural layer as attributes of the class
-	def __init__(self):
-		self.grad = []
-		self.weights =[] ; self.values = [] 
-
+class layer_data():  # This is a data class, I've stored data of each hidden neural layer as attributes of the class
+    def __init__(self,n1,n2):
+        self.grad = np.zeros((n1,n2))
+        self.bgrad = np.zeros((n2,))
+        self.weights = np.random.randn(n1,n2)
+        self.values = np.zeros((n1,)) 
+        self.bias = np.random.randn(n2,)
 
 class Network(layer_data):
     def __init__(self,layers,nodes,nin,nout): #Layers = int  || nodes,nin,nout are arrays
-        self.layers = layers;    self.nodes = nodes  # nodes doesn't just have info about hidden layers it composes of number of nodes in both input and ouput layers
+        self.layers = layers;    self.nodes = nodes  #Nodes will have info about only hidden layers
         self.nin = nin         
         self.nout  = nout
         self.data = ['n'+str(i) for i in range(layers)]
-        self.m,self.n = None   #Dimensions_clarification
         self.sig = lambda x: 1/(1+np.exp(-x))
-
-
-
-
-        
+        self.output = None
+        self.m,self.n = np.shape(nin)
 
     def sigmoid(self,other):
-        h = [i for i in map(self.sig,other)]
+        h = np.array([i for i in map(self.sig,other)])
         return h
     
+    def sigmoid_prime(self,other):
+        h = np.multiply((self.sigmoid(other)),(1-self.sigmoid(other)))
+        return h
 
-#   Cost funtion details -------------------------------------------------------------------------------------------------------------------------
+#Cost funtion details -------------------------------------------------------------------------------------------------------------------------
     def cost_function(self):
-        return #add cross entropy cost function
+        return np.sum(np.square(self.nout - self.output))*(1/self.m)
+    
+    def error(self):
+        return 2*sum(sum(self.nout - self.output))
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------
 
-    def initailize(self):
-        for i in self.data:
-            i = layer_data
-            if i != self.layers: i.weights = np.random.randn(self.nodes[i],self.nodes[i+1])
-        for i in range(1,self.layers):
-            self.data[i].values = self.sigmoid(np.matmul(self.data[i-1].values,self.data[i].weights))
+    def initailize(self):     #initialize random weights 
+        for j in (range(self.layers-1)):
+            self.data[j] = layer_data(self.nodes[j],self.nodes[j+1])    #Elements of the list data will store the computational details of each layer except last on.
+        self.data[0].bias = np.zeros((self.nodes[1],))                  #Changing bias term of the input layer, as we dont need to add bias term for the input layer.
+        self.data[0].values = self.nin
+
+#Gradient and weights are initialized as w0 would be weights for the mapping from 0 layer to 1st layer.
+#we shall store only the value details for the last layer.
 
     def feed_forward(self):
-        for i in range(1,self.layers):
-            self.data[i].values = self.sigmoid(np.matmul(self.data[i-1],self.data[i].weights))
+        for i in range(1,self.layers-1):
+            self.data[i].values = self.sigmoid(np.matmul(self.data[i-1].values,self.data[i-1].weights) + self.data[i-1].bias)
+        self.output = self.sigmoid(self.data[-2].values@self.data[-2].weights + self.data[-2].bias)
 
 #--------------------------------------------------------------------------------------------------------------------------------------------
     def update_values(self,alpha):
         for i in range(self.layers):
-            self.data[i].values = self.data[i].values - alpha*self.data[i].grad
-            self.data[i].grad = []
-
-    def compute_gradient(self):  #sigmoid function
-        pass                        #gradient i.e. the sensitivity of each node towards the output(or the cost function) is computed and fed into backpropagation
-    
+            self.data[i].weights = self.data[i].weights - (alpha/self.m)*self.data[i].grad
 
     def back_propagation(self): #cross entropy cost function is being used 
-        self.data[-1].grad = self.data[-1].values - self.nout
-        for i in np.arange(self.layers-2,-1):
-            self.data[i].grad = self.data[i+1].grad
+        self.data[-2].grad = self.error()*self.data[-2].weights
+        self.data[-2].bgrad = self.error()*self.data[-2].bias
+        print(self.data[-2].grad)
+        for i in np.arange(-3,-(self.layers+1),-1):
+            self.data[i].grad = np.multiply(self.data[i+1].grad,self.data[i].values)
+
+
+
+    def display(self):
+        for i in range(self.layers-1):
+            print("---------------------------------------------------------------------")
+            print(i)
+            print("Values = ",end=' ')
+            pprint(self.data[i].values)
+            print("Weights = ",end=' ')
+            pprint(self.data[i].weights)
+            print("Gradient = ",end=' ')
+            pprint(self.data[i].grad)
+            print("Bias =",end=' ')
+            pprint(self.data[i].bias)
+            print("Bgrad =",end=' ')
+            pprint(self.data[i].bgrad)
+            print("---------------------------------------------------------------------")
+        print("Output = ",end=' ')
+        pprint(self.output)
+        print("Main Error = ",end =' ')
+        pprint(self.error())
+
+    def display_shape(self):
+        for i in range(self.layers-1):
+            print("---------------------------------------------------------------------")
+            print(i)
+            print("Values = ",np.shape(self.data[i].values))
+            print("Weights = ",np.shape(self.data[i].weights))
+            print("Gradient = ",np.shape(self.data[i].grad))
+            print("Bias =",np.shape(self.data[i].bias))
+            print("Bgrad =",np.shape(self.data[i].bgrad))
+            print("---------------------------------------------------------------------")
+        print("Output = ",np.shape(self.output))
+        print("Main Error = ",np.shape(self.error()))
