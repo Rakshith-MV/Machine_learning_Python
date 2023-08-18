d0c75dd57deb16a96ff5963544e3790f20a9bb85
Rakshith <werakshith@gmail.com>
2023-08-18 20:05:52 +0530

    code.py

--- MNIST/src/code.py
+++ MNIST/src/code.py
@@ -1,81 +1,107 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-"""
-A Neural Network to classify digits(0-9), 
-Still very unsure about the number of neurons to use, I'll start with a 3 layered neuron with one hidden layer 
-I'll shall be using ReLu(Rectified linear unit) as the activation function(non linearity).
-MNIST data set, 54000 training examples 6000 cross validation and 10000 test data.
-"""
-
-"""SOME INFORMATION
-Data loaded
-Weights =  (784, 15)                                    Each epoch 
-Gradient =  (784, 15)                                           input = (6000,784)
-Bgradient =  (1, 15)                                            output = (6000,)
-Bias =  (1, 15)
-Weights =  (15, 1)
-Gradient =  (15, 1)
-Bgradient =  (1, 1)
-Bias =  (1, 1)
-"""
-
-#This class splits the data into batches and makes two arrays for training and cross_validation.
-class Batches():
-    def __init__(self,nin,length,batch_size):
+class Network():
+    def __init__(self,nin,nout,layers,nodes,cv,test,batch_size:int):
         self.nin = nin
-        self.length = length
-        self.batch_size = batch_size
-        self.training = []
-        self.cross_validation,self.cross_validation_output = [],[]
-        self.training_output = []
-#Just fucking use the reshape funciton you moron!!
-    def batch_split(self):
-        np.random.shuffle(self.nin)
-        epoch = int(self.length/self.batch_size)
-        start = 0
-        stop = self.batch_size
-        new_data = []
-        for i in range(epoch):
-            if i != epoch-1:
-                self.training.append(np.array(self.nin[start:stop,1:]))
-                self.training_output.append(np.array(self.nin[start:stop,0]))
-            else:
-                self.cross_validation.append(np.array(self.nin[start:stop,1:]))
-                self.cross_validation_output.append(np.array(self.nin[start:stop,0]))
-
-            start += self.batch_size
-            stop += self.batch_size
-
-class Info():
-    """
-    This class will hold all the infomartion required, the batch-sets, weights, gradients,..
-    the values are stored as arrays, i.e. weights[0] would be weights that would take from layer1 to layer 2 
-    """
-    def __init__(self,B,O,layers,neurons):
-        self.set = B
-        self.output = O
-        self.weights = [np.random.randn(neurons[i],neurons[i+1]) for i in range(layers-1)]
-        self.grad = [np.zeros((neurons[i],neurons[i+1])) for i in range(layers-1)]
-        self.bias = [np.zeros((1,neurons[i+1])) for i in range(layers-1)]
-        self.bgrad = [np.zeros((1,neurons[i+1])) for i in range(layers-1)]
-
-
-class Network(Batches):
-    def __init__(self,nin,length,layers,neurons):
-        self.inn = nin
-        self.size = length
+        self.m,self.n = np.shape(nin)   #m will be dimensions and n will be number of equations
         self.layers = layers
-        self.neurons = neurons
+        self.nodes = nodes
+        self.nout = nout
+        self.batch = batch_size
+        self.cross_validtion = cv
+        self.test_set = test
+#------------------------------------------------------------------------------------------------
+    def initialize(self):
+        self.weights = [0.01*np.random.rand(self.nodes[i+1],self.nodes[i]) for i in range(self.layers-1)]
+        self.grad = [np.zeros((self.nodes[i+1],self.nodes[i])) for i in range(self.layers-1)]
+        self.bias = [np.zeros((self.nodes[i+1],1)) for i in range(self.layers-1)]
+        self.bgrad = np.copy(self.bias)
+        self.activations = list(range(self.layers))
+        self.values = list(range(self.layers))
 
-    def initialize(self):           
-        split_data = Batches(self.inn,self.size,6000)   #Get the data to split into cross-validation and training set
-#!!!!!Work on batch size(6000)..
-        split_data.batch_split()                          #split_data.cross_validation for cross_validation set
-        data = Info(split_data.training,split_data.training_output,self.layers,self.neurons)    
-        print(np.shape(data.set[0]))
-        print(np.shape(data.output[0]))
+
+    def ReLu(self,other):
+        return np.maximum(0,other)
     
-#"-----------------------------------------------------------------------------------------------------------------------------------"
-#"-----------------------------------------------------------------------------------------------------------------------------------"
+    def softmax(self, Z):
+        expZ = np.exp(Z- np.max(Z))
+        return expZ / expZ.sum(axis=0, keepdims=True)
 
+    def cost_function(self,start):
+        return -np.mean(self.nout[:,start:start+self.batch]*np.log(self.activations[-1]+1e-8))
+    
+    def Deriv_Relu(self,x):
+        d = np.zeros_like(x)
+        d[x > 0] = 1
+        return d
+    
+    def plot(self,x,y,x1,y1):
+        plt.plot(x,y,xlabel=x1,ylabel=y1)
+#-----------------------------------------------------------------------------------------------
+
+    def feed_forward(self,x):
+        self.activations[0] = x
+        self.values[1] = self.weights[0].dot(self.activations[0])+self.bias[0]
+        self.activations[1] = self.ReLu(self.values[1])
+        self.values[2] = self.weights[1].dot(self.activations[1]) + self.bias[1]
+        self.activations[2] = self.softmax(self.values[2])
+
+    def error(self,start):
+        return (self.activations[2] - self.nout[:,start:start+self.batch] )
+    
+    def back_propagation(self,x,start):
+        E = self.error(start)
+        dz2 = E
+        self.grad[1] = dz2.dot(self.activations[1].T)/self.batch
+        self.bgrad[1] = np.sum(dz2)/self.batch
+    
+        dz1 = self.Deriv_Relu(self.values[1])*((self.weights[1].T).dot(dz2))  #derivative of relu of z1
+        self.grad[0] = dz1.dot(self.activations[0].T)/self.batch
+        self.bgrad[0] = np.sum(dz1)/self.batch
+
+    def update(self,a):
+        for j in range(self.layers-1):
+            self.weights[j] -= a*self.grad[j]
+            self.bias[j] -= a*self.bgrad[j]
+
+    def model(self,a,x,start):   
+        self.feed_forward(x)
+        self.back_propagation(x,start)
+        self.update(a)
+
+    def accuracy(self, two):
+        if two == False:
+            output = self.feed_forward(self.cross_validtion[0])
+            t,runs = 0,0
+            for i,j in zip(self.cross_validtion[1].T,self.activations[-1].T):
+                z = input()
+                if np.argmax(i) == np.argmax(j):
+                    t+=1
+                runs+=1
+            return t/runs
+        else:
+            output = self.feed_forward(self.test_set[0])
+            t,runs = 0,0
+            for i in zip(self.test_set[1].T,self.activations[-1].T):
+                if np.argmax(i) == np.argmax(j):
+                    t+=1
+                runs +=1
+            return t/runs
+
+
+    def train(self,a,iters):
+        cost = []
+        val = []
+        for k in range(iters):
+            i = 0
+            while(i<self.n):
+                x = self.nin[:,i:i+self.batch]
+                self.model(a,x,i)
+                val.append(i)
+                print(i)
+                cost.append(self.cost_function(i))
+                i+=self.batch
+            
+            if k%10 == 0:
+                print(k,self.accuracy(False))            
